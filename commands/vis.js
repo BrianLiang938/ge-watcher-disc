822981dea3855023a968c72bbb2a6c7a1a5b0b50
BrianLiang938 <brian.g.liang938@gmail.com>
2022-07-26 17:44:14 -0400

    chart formatting improved

--- commands/vis.js
+++ commands/vis.js
@@ -18,40 +18,34 @@ module.exports = {
 		const high = [];
 		const low = [];
 		const time = [];
-		for (let i = 0; i < 96; i++) {
+		for (let i = 0; i < 120; i++) {
 			const h = data[size - 1 - i].avgHighPrice;
 			const l = data[size - 1 - i].avgLowPrice;
 			const t = data[size - 1 - i].timestamp;
 			high.unshift(h);
 			low.unshift(l);
 			const date = new Date(t * 1000);
-			const hours = date.getHours();
-			const formattedTime = timeProcess(hours);
+			const formattedTime = timeProcess(date);
 			time.unshift(formattedTime);
 		}
 		const title = `Price History of Item ID ${input}`;
 		const chart = createGraph(low, high, title, time);
 		const url = await chart.getShortUrl();
-		console.log(url);
 		// https://quickchart.io/chart/render/zm-0bd78cfc-df6e-4d31-b1c1-1fd9d2ef7221?data1=${low}&data2=${high}&title=${title}&labels=${time}
 		const exampleEmbed = new MessageEmbed()
+			.setTitle(`${url}`)
 			.setImage(url);
 		interaction.channel.send({ embeds: [exampleEmbed] });
 	},
 };
 
-function timeProcess(hours) {
-	let formattedTime = hours + 'AM';
-	if (hours > 12) {
-		hours = hours - 12;
-		formattedTime = hours + 'PM';
-	}
-	else if (hours == 0) {
-		hours = 12;
-		formattedTime = hours + 'AM';
-	}
-	else if (hours == 12) {
-		formattedTime = hours + 'PM';
+function timeProcess(date) {
+	let formattedTime = '';
+	const hours = date.getHours();
+	const month = date.getMonth();
+	const day = date.getDate();
+	if (hours == 0) {
+		formattedTime = `${month}/${day}`;
 	}
 	return formattedTime;
 }
@@ -66,13 +60,18 @@ function createGraph(low, high, title, label) {
 				label: 'High Price',
 				data: high,
 				fill: false,
+				pointRadius: 0,
 			}, {
 				label: 'Low Price',
 				data: low,
 				fill: false,
+				pointRadius: 0,
 			}],
 		},
 		options: {
+			legend: {
+				display: false,
+			},
 			title: {
 				display: true,
 				text: title,
@@ -85,9 +84,6 @@ function createGraph(low, high, title, label) {
 				},
 				x: {
 					type: 'time',
-					time: {
-						unit: 'hour',
-					},
 					title: {
 						display: true,
 						text: 'Time',
@@ -99,17 +95,31 @@ function createGraph(low, high, title, label) {
 					min: Math.min(Math.min(...low), Math.min(...high)),
 					max: Math.max(Math.max(...low), Math.max(...high)),
 				},
-				yAxes: {
+				xAxes: [{
 					ticks: {
-						// Include a dollar sign in the ticks
-						callback: function(value) {
-							return '$' + value;
+						autoSkip: false,
+						maxRotation: 90,
+						minRoation: 90,
+					},
+				}],
+				yAxes: [{
+					ticks: {
+						callback: (val) => {
+							if (val > 1000000) {
+								val = val / 1000000;
+								return val.toLocaleString() + 'M';
+							}
+							if (val > 1000) {
+								val = val / 1000;
+								return val.toLocaleString() + 'K';
+							}
+							return val.toLocaleString();
 						},
 					},
-				},
+				}],
 			},
 		},
 	});
-	myChart.setBackgroundColor('rgb(192,192,192)').setWidth(800);
+	myChart.setBackgroundColor('rgb(192,192,192)').setWidth(1500).setHeight(600);
 	return myChart;
 }
